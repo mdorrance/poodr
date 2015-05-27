21465298dc0f27af28a5364942ef7e83674fa1ce
Michael Dorrance <dorrancemd@gmail.com>
2015-05-27 07:33:27 -0600

    adds 15 classes

---
+++ poodr.rb
@@ -0,0 +1,62 @@
+#Lesson 1 of Practical Object-Oriented Design in Ruby
+
+class FuelTank
+
+end
+
+class LandingGear
+
+end
+
+class Fusalage
+
+end
+
+class FirstClassSeat
+
+end
+
+class BusinessClassSeat
+
+end
+
+class EconomySeat
+
+end
+
+class PassengerWindows
+
+end
+
+class Windshield
+
+end
+
+class AutoPilot
+
+end
+
+class NavigationSystem
+
+end
+
+class ControlSystem
+
+end
+
+class LavToilet
+
+end
+
+class LavSink
+
+end
+
+class OxygenMasks
+
+end
+
+class Tires
+
+end
+
